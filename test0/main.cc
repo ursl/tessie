7dbced7557beba1fbff62b7d539a07420b2db9ab
Urs Langenegger <urslangenegger@gmail.com>
2020-07-15 11:21:18 +0200

    allow fullscreen (default) and window (-w) mode for starting up

--- test0/main.cc
+++ test0/main.cc
@@ -1,13 +1,32 @@
+#include <iostream>
 #include <QtWidgets/QApplication>
 
 #include "gui.hh"
 
+using namespace std;
+
 int main(int argc, char *argv[]) {
+  // -- by default run in full screen mode
+  bool doRunInWindow(false);
+
+  for (int i = 0; i < argc; i++){
+    if (!strcmp(argv[i],"-h")) {
+      cout << "List of arguments:" << endl;
+      cout << "-w                    start in window (not fullscreen)" << endl;
+      return 0;
+    }
+    if (!strcmp(argv[i], "-w")) {doRunInWindow = true;}
+  }
+
+
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
   QApplication app(argc, argv);
 
   gui theGui;
+  if (!doRunInWindow) {
+    theGui.showFullScreen();
+  }
 
   return app.exec();
 }
