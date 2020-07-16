d19342ccd9d00a3fe6ab9c1869f6926c0df8f9e7
Urs Langenegger <urslangenegger@gmail.com>
2020-07-16 16:39:09 +0200

    new logger class

---
+++ test0/tLog.cc
@@ -0,0 +1,120 @@
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+
+#include "tLog.hh"
+#include "gui.hh"
+
+using namespace std;
+
+// ----------------------------------------------------------------------
+tLog::tLog(string fname): fLevel(DEBUG), fFileName(fname) {
+  fFile.open(fFileName, ios_base::app);
+  int filesize = fFile.tellp();
+  if (filesize > 1000) {
+    cout << "filesize = " << filesize << " too large, creating new logfile" << endl;
+    fFile.close();
+    string bacname = fFileName + string(".") + timeStamp(true);
+    int result = rename(fFileName.c_str(), bacname.c_str());
+    cout << "renamed " << fFileName << " to " << bacname << " with result = " << result << endl;
+    fFile.open(fFileName, ios_base::app);
+  }
+}
+
+// ----------------------------------------------------------------------
+tLog:: ~tLog() {
+  fFile.close();
+}
+
+// ----------------------------------------------------------------------
+string tLog::print(tLogLevel level, std::string print) {
+  string slevel = toString(level);
+  stringstream output;
+  output << timeStamp() << " "
+	 << std::setfill(' ') << std::setw(7) << slevel << " "
+	 << print;
+  string sout = output.str();
+  fFile << sout << endl;
+  if (level <= fLevel) {
+    fpHw->printToGUI(sout);
+  }
+  return sout;
+}
+
+// ----------------------------------------------------------------------
+void tLog::operator()(tLogLevel level, std::string print) {
+  string slevel = toString(level);
+  stringstream output;
+  output << timeStamp() << " "
+	 << std::setfill(' ') << std::setw(7) << slevel << " "
+	 << print;
+  string sout = output.str();
+  fFile << sout << endl;
+  if (level <= fLevel) {
+    fpGui->printText(sout);
+  }
+}
+
+
+
+// ----------------------------------------------------------------------
+tLogLevel tLog::getLevel() {
+  return fLevel;
+}
+
+// ----------------------------------------------------------------------
+void tLog::setLevel(string level) {
+  fLevel = fromString(level);
+}
+
+
+// ----------------------------------------------------------------------
+std::string tLog::toString(tLogLevel level) {
+  static const char* const buffer[] = {"ERROR", "WARNING", "INFO", "DEBUG", "ALL"};
+  return buffer[level];
+}
+
+// ----------------------------------------------------------------------
+tLogLevel tLog::fromString(const std::string& level) {
+  if (level == "DEBUG")
+    return DEBUG;
+  if (level == "INFO")
+    return INFO;
+  if (level == "WARNING")
+    return WARNING;
+  if (level == "ERROR")
+    return ERROR;
+  if (level == "ALL")
+    return ALL;
+  // give up
+  return ALL;
+}
+
+
+
+// ----------------------------------------------------------------------
+string tLog::timeStamp(bool filestamp) {
+  char buffer[11];
+  time_t t;
+  time(&t);
+  tm r;
+  strftime(buffer, sizeof(buffer), "%X", localtime_r(&t, &r));
+  struct timeval tv;
+  gettimeofday(&tv, 0);
+
+  tm *ltm = localtime(&t);
+  int year  = 1900 + ltm->tm_year;
+  int month = 1 + ltm->tm_mon;
+  int day   = 1 + ltm->tm_mday;
+  int hour  = ltm->tm_hour;
+  int min   = ltm->tm_min;
+  int sec   = ltm->tm_sec;
+  std::stringstream result;
+  if (filestamp) {
+    result << year  << std::setfill('0') << std::setw(2) << month << day << "-" << hour << min << sec;
+    return result.str();
+  }
+  result << year << "/" << std::setfill('0') << std::setw(2) << month << "/" << day << " "
+	 << buffer << "." << std::setfill('0') << std::setw(3) << ((long)tv.tv_usec / 1000);
+  return result.str();
+}
---
+++ test0/tLog.hh
@@ -0,0 +1,44 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <iomanip>
+#include <sys/time.h>
+
+inline std::string NowTime();
+
+class gui;
+class driveHardware;
+
+enum tLogLevel {ERROR, WARNING, INFO, DEBUG, ALL};
+
+class tLog {
+public:
+  tLog(std::string fname = "tessie.txt");
+  virtual ~tLog();
+public:
+  void setLevel(std::string);
+  tLogLevel getLevel();
+  void operator()(tLogLevel, std::string);
+  // -- same as above but also returns the string produced
+  std::string print(tLogLevel, std::string);
+
+  static std::string toString(tLogLevel level);
+  static tLogLevel fromString(const std::string& level);
+  std::string timeStamp(bool filestamp = false);
+  void setGui(gui *x) {fpGui = x;}
+  void setHw(driveHardware *x) {fpHw = x;}
+private:
+  tLog(const tLog&);
+  tLog& operator = (const tLog&);
+
+  gui *fpGui;
+  driveHardware *fpHw;
+  tLogLevel fLevel;
+  std::string fFileName;
+  std::ofstream fFile;
+};
+
+#endif
