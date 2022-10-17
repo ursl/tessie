da0e677f9503478eeedc479dc57dbe94da70616e
Urs Langenegger <urslangenegger@gmail.com>
2022-10-17 17:48:03 +0200

    add dbx printout

--- test1/tessie.cpp
+++ test1/tessie.cpp
@@ -8,8 +8,12 @@ int main(int argc, char *argv[]) {
   tLog LOG;
 
   QApplication a(argc, argv);
+  std::cout << "MainWindow w() call" << std::endl;
   MainWindow w(LOG, nullptr);
+
+  std::cout << "LOG.setGUI(&w) call" << std::endl;
   LOG.setGui(&w);
+
   std::cout << "w.show() call" << std::endl;
   w.show();
   std::cout << "w.show() done" << std::endl;
