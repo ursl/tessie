334d32544bee3fc6e66bad6d555ba9a92bd30224
Urs Langenegger <urslangenegger@gmail.com>
2022-10-20 10:15:04 +0200

    wait a blink

--- test1/driveHardware.cc
+++ test1/driveHardware.cc
@@ -276,7 +276,7 @@ void driveHardware::readCANmessage() {
 
   //if (DBX) cout << "try to call read for fCANId = 0x" << hex << fCANId << dec << endl;
 
-  nbytes = read(fSr, &fFrameR, sizeof(fFrameR));
+  nbytes = read(fSw, &fFrameR, sizeof(fFrameR));
   bool RegSend = (fFrameR.can_id & 0x040) && (0 == (fFrameR.can_id & 0x030));
   cout << "RegSend = " << RegSend <<  " fFrameR.can_id = " << hex << fFrameR.can_id << endl;
 
