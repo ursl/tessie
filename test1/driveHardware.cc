751c8ad1c6dd51762661386652fe273e18611339
Urs Langenegger <urslangenegger@gmail.com>
2022-10-20 09:59:51 +0200

    dbx

--- test1/driveHardware.cc
+++ test1/driveHardware.cc
@@ -286,6 +286,8 @@ void driveHardware::readCANmessage() {
           printf("%3d ", fFrameR.data[i]);
       }
 
+      cout << " itec = " << itec << " (fCANReg = " << fCANReg << ") ";
+
       itec    = fFrameR.can_id & 0xf;
       ireg    = fFrameR.data[0];
       data[0] = fFrameR.data[1];
@@ -643,10 +645,10 @@ void driveHardware::readAllParamsFromCAN() {
 
   cout << "driveHardware::readAllParamsFromCAN() read Temp_M" << endl;
   for (int i = 1; i <= 8; ++i) fTECData[i].reg["Temp_M"].value = getTECRegisterFromCAN(i, "Temp_M");
+  return;
 
   cout << "driveHardware::readAllParamsFromCAN() read ControlVoltage_Set" << endl;
   for (int i = 1; i <= 8; ++i) fTECData[i].reg["ControlVoltage_Set"].value = getTECRegisterFromCAN(i, "ControlVoltage_Set");
-  return;
 
   cout << "driveHardware::readAllParamsFromCAN() read PID_kp" << endl;
   for (int i = 1; i <= 8; ++i) fTECData[i].reg["PID_kp"].value = getTECRegisterFromCAN(i, "PID_kp");
