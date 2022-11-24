73c9401f254a89d1e8f3507167f489aaabca045e
Urs Langenegger <urslangenegger@gmail.com>
2022-11-24 11:31:31 +0100

    migrate to map<int, map<int, deque<canFrame>>>

--- test1/CANmessage.cc
+++ test1/CANmessage.cc
@@ -16,18 +16,32 @@ void CANmessage::addFrame(canFrame &x) {
   //  }
   if (0 == x.fFRAS) {
     fFrames.push_back(x);
+  } else if (x.fAlarm > 0) {
+    fqAlarmFrames.push_back(x);
   } else {
     fFRASFrames.push_back(x);
+    fqFRASFrames.push_back(x);
   }
 }
 
 
 // ----------------------------------------------------------------------
 void CANmessage::dump() {
+  cout << "dump vector containers" << endl;
   for (unsigned int i = 0; i < fFrames.size(); ++i) {
     fFrames[i].dump();
   }
+
+  cout << "dump map<int, map<int, deque>> containers" << endl;
+  for (auto &itt :fMapFrames) {
+    for (auto &itd: itt.second) {
+      for (auto &itc: itd.second) {
+        itc.dump();
+      }
+    }
+  }
 }
+
 
 // ----------------------------------------------------------------------
 unsigned int CANmessage::nFrames() {
@@ -36,33 +50,43 @@ unsigned int CANmessage::nFrames() {
 
 
 // ----------------------------------------------------------------------
+unsigned int CANmessage::nFrames(int itec) {
+  return fMapFrames[itec].size();
+}
+
+
+// ----------------------------------------------------------------------
+unsigned int CANmessage::nFrames(int itec, int ireg) {
+  return fMapFrames[itec][ireg].size();
+}
+
+// ----------------------------------------------------------------------
 int CANmessage::nErrors() {
   return fErrorCounter;
 }
 
 // ----------------------------------------------------------------------
-float CANmessage::getFloat(unsigned int tec, unsigned int reg) {
+float CANmessage::getFloat(unsigned int itec, unsigned int ireg) {
   float result(-98.);
-  // -- find the last one
-  for (std::vector<canFrame>::iterator it = fFrames.begin(); it != fFrames.end(); ++it) {
-    if ((tec == it->fTec) && (reg == it->fReg)) {
-      result = it->fFloatVal;
-      //fFrames.erase(it);
-      //break;
-    }
+
+  if (nFrames(itec, ireg) > 0) {
+    result = fMapFrames[itec][ireg].front().fFloatVal;
+    cout << " pop_front "; fMapFrames[itec][ireg].front().dump(false); cout << endl;
+    fMapFrames[itec][ireg].pop_front();
   }
+
   if (result < -90.) {
     ++fErrorCounter;
-    cout << "Error: reg " << reg << " itec " << tec << " getFloat " << result << endl;
+    cout << "Error: reg " << ireg << " itec " << itec << " getFloat " << result << endl;
   }
   return result;
 }
 
 // ----------------------------------------------------------------------
-int CANmessage::getInt(unsigned int tec, unsigned int reg) {
+int CANmessage::getIntV(unsigned int tec, unsigned int reg) {
   int result(-98);
   // -- find the last one
-  for (std::vector<canFrame>::iterator it = fFrames.begin(); it != fFrames.end(); ++it) {
+  for (vector<canFrame>::iterator it = fFrames.begin(); it != fFrames.end(); ++it) {
     if ((tec == it->fTec) && (reg == it->fReg)) {
       result = it->fIntVal;
       //fFrames.erase(it);
@@ -78,7 +102,25 @@ int CANmessage::getInt(unsigned int tec, unsigned int reg) {
 
 
 // ----------------------------------------------------------------------
-int CANmessage::getFRASMessage() {
+int CANmessage::getInt(unsigned int itec, unsigned int ireg) {
+  int result(-98);
+
+  if (nFrames(itec, ireg) > 0) {
+    result = fMapFrames[itec][ireg].front().fIntVal;
+    cout << " pop_front "; fMapFrames[itec][ireg].front().dump(false); cout << endl;
+    fMapFrames[itec][ireg].pop_front();
+  }
+
+  if (result < -90.) {
+    ++fErrorCounter;
+    cout << "Error: reg " << ireg << " itec " << itec << " getInt " << result << endl;
+  }
+  return result;
+}
+
+
+// ----------------------------------------------------------------------
+int CANmessage::getFRASMessageV() {
   int result = fFRASFrames.size();
   if (result > 0) fFRASFrames.clear();
   return result;
@@ -86,7 +128,16 @@ int CANmessage::getFRASMessage() {
 
 
 // ----------------------------------------------------------------------
-int CANmessage::getAlarm() {
+int CANmessage::getFRASMessage() {
+  int result = fqFRASFrames.size();
+  // -- clear it
+  if (result > 0) fqFRASFrames.pop_front();
+  return result;
+}
+
+
+// ----------------------------------------------------------------------
+int CANmessage::getAlarmV() {
   int result(0);
   // -- find the last one
   for (std::vector<canFrame>::iterator it = fFrames.begin(); it != fFrames.end(); ++it) {
@@ -95,5 +146,16 @@ int CANmessage::getAlarm() {
     }
   }
 
+  return result;
+}
+
+
+// ----------------------------------------------------------------------
+int CANmessage::getAlarm() {
+  int result(0);
+  if (fqAlarmFrames.size() > 0) {
+    result = fqAlarmFrames.front().fAlarm;
+    fqAlarmFrames.pop_front();
+  }
   return result;
 }
--- test1/CANmessage.hh
+++ test1/CANmessage.hh
@@ -2,6 +2,8 @@
 #define CANMESSAGE_HH
 
 #include <vector>
+#include <deque>
+#include <map>
 
 #include "canFrame.hh"
 
@@ -10,26 +12,42 @@ public:
   CANmessage();
 
   // -- number of frames held
-  unsigned int nFrames();
+  unsigned int nFrames(); // vectors REMOVE
+  unsigned int nFrames(int itec);
+  unsigned int nFrames(int itec, int ireg);
+
   // -- get errors encountered
   int nErrors();
   // -- clear frames
-  void clearFrames() {fFrames.clear();}
+  void clearFrames() {fFrames.clear();} // vectors REMOVE
+  void clearAllFrames() {fMapFrames.clear();}
   // -- print
   void dump();
+
+  void addFrameV(canFrame &x);
   void addFrame(canFrame &x);
+
   // -- read float and erase frame
+  float getFloatV(unsigned int itec, unsigned int ireg);
   float getFloat(unsigned int itec, unsigned int ireg);
   // -- read int and erase frame
+  int getIntV(unsigned int itec, unsigned int ireg);
   int getInt(unsigned int itec, unsigned int ireg);
-  // -- check for FRAS message (0x42)
+  // -- check for FRAS message (0x42), returns 1 (0) if there is one (none)
+  int getFRASMessageV();
   int getFRASMessage();
   // -- check for alarm
+  int getAlarmV();
   int getAlarm();
 
 private:
   std::vector<canFrame> fFrames;
   std::vector<canFrame> fFRASFrames;
+
+  // map<tec, map<reg, deque<canFrame>>>
+  std::map<int, std::map<int, std::deque<canFrame>>> fMapFrames;
+  std::deque<canFrame>                 fqFRASFrames;
+  std::deque<canFrame>                 fqAlarmFrames;
 
   int fErrorCounter;
 };
