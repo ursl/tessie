9e0847e2e52af3399190dc7e98cea0640f9b6a23
Urs Langenegger <urslangenegger@gmail.com>
2022-11-24 15:09:22 +0100

    msec, not usec

--- test1/driveHardware.cc
+++ test1/driveHardware.cc
@@ -938,7 +938,7 @@ string driveHardware::timeStamp(bool filestamp) {
   result << std::setfill('0') << std::setw(2) << hour << ":"
          << std::setfill('0') << std::setw(2) << min << ":"
          << std::setfill('0') << std::setw(2) << sec << "."
-         << std::setfill('0') << std::setw(6) << ((long)tv.tv_usec);
+         << std::setfill('0') << std::setw(6) << ((long)tv.tv_usec)/1000;
   return result.str();
 }
 
