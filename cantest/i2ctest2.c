060f3e36bd1b0d77c39bafe4190d369b562eadbf
Urs Langenegger <urslangenegger@gmail.com>
2023-05-16 14:41:02 +0200

    read installed vprobe

--- cantest/i2ctest2.c
+++ cantest/i2ctest2.c
@@ -6,8 +6,27 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 
-#define I2C_ADDR 0x3f    // i2c address of 
+#define I2C_ADDR1 0x3e    // i2c address of 
+#define I2C_ADDR2 0x3f    // i2c address of 
 
+// ----------------------------------------------------------------------
+void readI2c(int file) {
+  // -- read 16 bytes of data
+  char data[16] = {0};
+  int readl = read(file, data, 16); 
+  if (readl != 16) {
+    printf("Error : Input/output Error readl = %d\n", readl);
+  } else {
+    printf("Bytes read: ");
+    for (int i = 0; i < 16; i = i+2) {
+      printf("%02x%02x ", data[i], data[i+1]);
+    }
+    printf("\n");          
+  }
+}
+
+
+// ----------------------------------------------------------------------
 int main(int argc, char *argv[]) {
 
   // -- create I2C bus
@@ -18,21 +37,11 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  // -- get I2C device, SHT85 I2C address is 0x44
-  ioctl(file, I2C_SLAVE, I2C_ADDR);
-  
-  // -- read 16 bytes of data
-  char data[16] = {0};
-  int readl = read(file, data, 16); 
-  if (readl != 16) {
-    printf("Error : Input/output Error readl = %d\n", readl);
-  } else {
-    printf("Bytes read: ");
-    for (int i = 0; i < 16; ++i) {
-      printf("%2x ", data[i]);
-    }
-    printf("\n");          
-  }
-  
+  ioctl(file, I2C_SLAVE, I2C_ADDR1);
+  readI2c(file); 
+
+  ioctl(file, I2C_SLAVE, I2C_ADDR2);
+  readI2c(file); 
+
   return 0;
 }
