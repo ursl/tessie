c856f25469b72fa378244c54c74ce4947361bee6
Urs Langenegger <urslangenegger@gmail.com>
2023-05-16 11:55:17 +0200

    add ALARM to MQTT alarm messages

--- test1/driveHardware.cc
+++ test1/driveHardware.cc
@@ -275,7 +275,7 @@ void driveHardware::doRun() {
 void driveHardware::ensureSafety() {
   // -- air temperatures
   if (fSHT85Temp > SAFETY_MAXSHT85TEMP) {
-    stringstream a("Box air temperature = " +
+    stringstream a("==ALARM== Box air temperature = " +
                    to_string(fSHT85Temp) +
                    " exceeds SAFETY_MAXSHT85TEMP = " +
                    to_string(SAFETY_MAXSHT85TEMP));
@@ -287,7 +287,7 @@ void driveHardware::ensureSafety() {
 
   // -- dew point vs air temperature
   if ((fSHT85Temp - SAFETY_DPMARGIN) < fSHT85DP) {
-    stringstream a("Box air temperature = " +
+    stringstream a("==ALARM== Box air temperature = " +
                     to_string(fSHT85Temp) +
                     " is too close to dew point = " +
                     to_string(fSHT85DP));
@@ -299,7 +299,7 @@ void driveHardware::ensureSafety() {
 
   // -- check water temperature
   if (fTECData[8].reg["Temp_W"].value > SAFETY_MAXTEMPW) {
-    stringstream a("Water temperature = " +
+    stringstream a("==ALARM== Water temperature = " +
                     to_string(fTECData[8].reg["Temp_W"].value) +
                     " exceeds SAFETY_MAXTEMPW = " +
                     to_string(SAFETY_MAXTEMPW));
@@ -313,7 +313,7 @@ void driveHardware::ensureSafety() {
   for (int itec = 1; itec <= 8; ++itec) {
     double mtemp = fTECData[itec].reg["Temp_M"].value;
     if (mtemp > SAFETY_MAXTEMPM) {
-      stringstream a("module temperature = " +
+      stringstream a("==ALARM== module temperature = " +
                       to_string(mtemp) +
                       " exceeds SAFETY_MAXTEMPM = " +
                       to_string(SAFETY_MAXTEMPM));
@@ -324,7 +324,7 @@ void driveHardware::ensureSafety() {
     }
 
     if ((mtemp - SAFETY_DPMARGIN) < fSHT85DP) {
-      stringstream a("module " + to_string(itec) + " temperature = " +
+      stringstream a("==ALARM== module " + to_string(itec) + " temperature = " +
                      to_string(mtemp) +
                      " is too close to dew point = " +
                      to_string(fSHT85DP)
