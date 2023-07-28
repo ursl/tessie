7e67418e6bd237ca751aa6352098ae9b056d4e42
Urs Langenegger <urslangenegger@gmail.com>
2023-07-28 17:47:56 +0200

    another wrap up, not yet solving the underlying problem

--- node/app1.js
+++ node/app1.js
@@ -1,9 +1,26 @@
+const fs = require('fs').promises;
 const express = require('express');
 const app = express();
 const port = 80
 
-app.get('/', (req, res) => {
-    res.send('app1: Hello World!')
+const mqtt = require('mqtt');
+const client = mqtt.connect('mqtt://coldbox01.psi.ch');
+
+mqttMessage = "";
+
+client.subscribe('monTessie');
+client.on('message', (topic, message) => {
+    console.log(`Received message on topic ${topic}: ${message}`);
+    mqttMessage = message;
+    //  res.send(`Received message on topic ${topic}: ${message}`);
+});
+
+
+
+app.get('/data', (req, res) => {
+    console.log(`app1 received data request`)
+//    mqttMessageJson = `\{\"MQTT\" : \"title\": \"Titel\", \"data\": \{${mqttMessage}\}\}`;
+//    data=> res.json(mqttMessageJson);
 })
 
 
@@ -13,7 +30,13 @@ app.listen(port, () => {
 
 
 app.get('/tessie/', (req, res) => {
-  res.send('Got a tessie request')
+    //    res.send("tessie!");
+    fs.readFile(__dirname + "/index2.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
 })
 
 
