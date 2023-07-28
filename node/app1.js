39d98a22e8984b809c043564c0a8064d984873e6
Urs Langenegger <urslangenegger@gmail.com>
2023-07-28 15:53:54 +0200

    updated app1 to respond to /tessie

--- node/app1.js
+++ node/app1.js
@@ -1,28 +1,19 @@
 const express = require('express');
 const app = express();
+const port = 80
 
-var client = null;
+app.get('/', (req, res) => {
+    res.send('app1: Hello World!')
+})
 
-app.get('/subscribe', (req, res) => {
-    // send headers to keep connection alive
-    const headers = {
-        'Content-Type': 'text/event-stream',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'no-cache'
-    };
-    res.writeHead(200, headers);
-    
-    // send client a simple response
-    res.write('you are subscribed');
-    
-    // store `res` of client to let us send events at will
-    client = res;
-    
-    // listen for client 'close' requests
-    req.on('close', () => { client = null; })
-});
 
-// send refresh event (must start with 'data: ')
-function sendRefresh() {
-    client.write('data: refresh');
-}
+app.listen(port, () => {
+  console.log(`app1 listening on port ${port}`)
+})
+
+
+app.get('/tessie/', (req, res) => {
+  res.send('Got a tessie request')
+})
+
+
