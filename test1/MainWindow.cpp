c456d86abc373fead1518760765706fa4e1e2bbb
Urs Langenegger <urslangenegger@gmail.com>
2023-07-28 17:49:17 +0200

    try to get Supply_U in lower row visible again

--- test1/MainWindow.cpp
+++ test1/MainWindow.cpp
@@ -21,7 +21,7 @@ MainWindow::MainWindow(tLog &x, driveHardware *h, QWidget *parent) :
   fTECDisplay->close();
   fTECDisplay->setHardware(fpHw);
 
-  ui->labelVersion->setText("2023/07/21-03");
+  ui->labelVersion->setText("2023/07/28-01");
   //ui->labelStatus->setText("OK");
 
   ui->lineEditRunTime->setAlignment(Qt::AlignRight);
