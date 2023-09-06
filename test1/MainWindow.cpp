9ec1442f550cf59011369736bde22dfc99d629e0
Urs Langenegger <urslangenegger@gmail.com>
2023-09-06 10:11:26 +0200

    back to odyssee

--- test1/MainWindow.cpp
+++ test1/MainWindow.cpp
@@ -32,6 +32,8 @@ MainWindow::MainWindow(tLog &x, driveHardware *h, QWidget *parent) :
   ui->lineEditRH->setAlignment(Qt::AlignRight);
   ui->lineEditDP->setAlignment(Qt::AlignRight);
 
+  ui->horizontalGroupBox_2->setAlignment(Qt::AlignCenter);
+
   fUICheckBox.push_back(ui->checkBoxTEC1);
   fUICheckBox.push_back(ui->checkBoxTEC2);
   fUICheckBox.push_back(ui->checkBoxTEC3);
