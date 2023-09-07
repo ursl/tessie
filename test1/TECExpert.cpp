3796892fd139aebd87b3e901cb91516aa0232a7a
Urs Langenegger <urslangenegger@gmail.com>
2023-09-07 11:28:22 +0200

    full matrix

--- test1/TECExpert.cpp
+++ test1/TECExpert.cpp
@@ -58,7 +58,15 @@ TECExpert::TECExpert(MainWindow *m, driveHardware *x) : fThread(x), fUI(0), fMW(
         switch (iy) {
           case 1:
             fMapTecMode.insert(make_pair(ix, pte));
-            QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecModeSet()));
+            if (1 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec1ModeSet()));
+            if (2 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec2ModeSet()));
+            if (3 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec3ModeSet()));
+            if (4 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec4ModeSet()));
+            if (5 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec5ModeSet()));
+            if (6 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec6ModeSet()));
+            if (7 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec7ModeSet()));
+            if (8 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec8ModeSet()));
+
             break;
           case 2:
             fMapTecControlVoltageSet.insert(make_pair(ix, pte));
@@ -73,27 +81,74 @@ TECExpert::TECExpert(MainWindow *m, driveHardware *x) : fThread(x), fUI(0), fMW(
             break;
           case 3:
             fMapTecPIDkp.insert(make_pair(ix, pte));
-            QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecPIDkpSet()));
+            if (1 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec1PIDkpSet()));
+            if (2 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec2PIDkpSet()));
+            if (3 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec3PIDkpSet()));
+            if (4 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec4PIDkpSet()));
+            if (5 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec5PIDkpSet()));
+            if (6 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec6PIDkpSet()));
+            if (7 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec7PIDkpSet()));
+            if (8 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec8PIDkpSet()));
             break;
           case 4:
             fMapTecPIDki.insert(make_pair(ix, pte));
-            QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecPIDkiSet()));
+            // QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecPIDkiSet()));
+            if (1 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec1PIDkiSet()));
+            if (2 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec2PIDkiSet()));
+            if (3 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec3PIDkiSet()));
+            if (4 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec4PIDkiSet()));
+            if (5 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec5PIDkiSet()));
+            if (6 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec6PIDkiSet()));
+            if (7 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec7PIDkiSet()));
+            if (8 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec8PIDkiSet()));
             break;
           case 5:
             fMapTecPIDkd.insert(make_pair(ix, pte));
-            QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecPIDkdSet()));
+            // QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecPIDkdSet()));
+            if (1 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec1PIDkdSet()));
+            if (2 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec2PIDkdSet()));
+            if (3 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec3PIDkdSet()));
+            if (4 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec4PIDkdSet()));
+            if (5 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec5PIDkdSet()));
+            if (6 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec6PIDkdSet()));
+            if (7 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec7PIDkdSet()));
+            if (8 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec8PIDkdSet()));
             break;
           case 6:
             fMapTecPIDMax.insert(make_pair(ix, pte));
-            QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecPIDMaxSet()));
+            // QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecPIDMaxSet()));
+            if (1 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec1PIDMaxSet()));
+            if (2 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec2PIDMaxSet()));
+            if (3 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec3PIDMaxSet()));
+            if (4 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec4PIDMaxSet()));
+            if (5 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec5PIDMaxSet()));
+            if (6 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec6PIDMaxSet()));
+            if (7 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec7PIDMaxSet()));
+            if (8 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec8PIDMaxSet()));
             break;
           case 7:
-            fMapTecPIDMin.insert(make_pair(ix, pte));
-            QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecPIDMinSet()));
+            fMapTecRefU.insert(make_pair(ix, pte));
+            //QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecPIDMinSet()));
+            if (1 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec1PIDMinSet()));
+            if (2 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec2PIDMinSet()));
+            if (3 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec3PIDMinSet()));
+            if (4 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec4PIDMinSet()));
+            if (5 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec5PIDMinSet()));
+            if (6 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec6PIDMinSet()));
+            if (7 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec7PIDMinSet()));
+            if (8 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec8PIDMinSet()));
             break;
           case 8:
             fMapTecRefU.insert(make_pair(ix, pte));
-            QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecRefUSet()));
+            // QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tecRefUSet()));
+            if (1 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec1RefUSet()));
+            if (2 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec2RefUSet()));
+            if (3 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec3RefUSet()));
+            if (4 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec4RefUSet()));
+            if (5 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec5RefUSet()));
+            if (6 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec6RefUSet()));
+            if (7 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec7RefUSet()));
+            if (8 == ix) QObject::connect(pte, SIGNAL(returnPressed()), this, SLOT(tec8RefUSet()));
             break;
 
           default:
@@ -148,78 +203,78 @@ void TECExpert::tecSetFromUI(int itec, std::string rname, QLineEdit *ql) {
 //}
 
 
-// -------------------------------------------------------------------------------
-void TECExpert::tecModeSet() {
-  for(auto it: fMapTecMode) {
-    QString sval = it.second->text();
-    float xval = sval.toFloat();
-    cout << "xval = " << xval << " it.first = " << it.first << endl;
-    fThread->setTECRegister(it.first, "Mode", xval);
-  }
-}
+//// -------------------------------------------------------------------------------
+//void TECExpert::tecModeSet() {
+//  for(auto it: fMapTecMode) {
+//    QString sval = it.second->text();
+//    float xval = sval.toFloat();
+//    cout << "xval = " << xval << " it.first = " << it.first << endl;
+//    fThread->setTECRegister(it.first, "Mode", xval);
+//  }
+//}
 
 
-// -------------------------------------------------------------------------------
-void TECExpert::tecPIDkpSet() {
-  for(auto it: fMapTecControlVoltageSet) {
-    QString sval = it.second->text();
-    float xval = sval.toFloat();
-    cout << "xval = " << xval << " it.first = " << it.first << endl;
-    fThread->setTECRegister(it.first, "PID_kp", xval);
-  }
-}
+//// -------------------------------------------------------------------------------
+//void TECExpert::tecPIDkpSet() {
+//  for(auto it: fMapTecControlVoltageSet) {
+//    QString sval = it.second->text();
+//    float xval = sval.toFloat();
+//    cout << "xval = " << xval << " it.first = " << it.first << endl;
+//    fThread->setTECRegister(it.first, "PID_kp", xval);
+//  }
+//}
 
 
-// -------------------------------------------------------------------------------
-void TECExpert::tecPIDkiSet() {
-  for(auto it: fMapTecControlVoltageSet) {
-    QString sval = it.second->text();
-    float xval = sval.toFloat();
-    cout << "xval = " << xval << " it.first = " << it.first << endl;
-    fThread->setTECRegister(it.first, "PID_ki", xval);
-  }
-}
+//// -------------------------------------------------------------------------------
+//void TECExpert::tecPIDkiSet() {
+//  for(auto it: fMapTecControlVoltageSet) {
+//    QString sval = it.second->text();
+//    float xval = sval.toFloat();
+//    cout << "xval = " << xval << " it.first = " << it.first << endl;
+//    fThread->setTECRegister(it.first, "PID_ki", xval);
+//  }
+//}
 
 
-// -------------------------------------------------------------------------------
-void TECExpert::tecPIDkdSet() {
-  for(auto it: fMapTecControlVoltageSet) {
-    QString sval = it.second->text();
-    float xval = sval.toFloat();
-    cout << "xval = " << xval << " it.first = " << it.first << endl;
-    fThread->setTECRegister(it.first, "PID_kd", xval);
-  }
-}
+//// -------------------------------------------------------------------------------
+//void TECExpert::tecPIDkdSet() {
+//  for(auto it: fMapTecControlVoltageSet) {
+//    QString sval = it.second->text();
+//    float xval = sval.toFloat();
+//    cout << "xval = " << xval << " it.first = " << it.first << endl;
+//    fThread->setTECRegister(it.first, "PID_kd", xval);
+//  }
+//}
 
 
-// -------------------------------------------------------------------------------
-void TECExpert::tecPIDMaxSet() {
-  for(auto it: fMapTecControlVoltageSet) {
-    QString sval = it.second->text();
-    float xval = sval.toFloat();
-    cout << "xval = " << xval << " it.first = " << it.first << endl;
-    fThread->setTECRegister(it.first, "PID_Max", xval);
-  }
-}
+//// -------------------------------------------------------------------------------
+//void TECExpert::tecPIDMaxSet() {
+//  for(auto it: fMapTecControlVoltageSet) {
+//    QString sval = it.second->text();
+//    float xval = sval.toFloat();
+//    cout << "xval = " << xval << " it.first = " << it.first << endl;
+//    fThread->setTECRegister(it.first, "PID_Max", xval);
+//  }
+//}
 
 
-// -------------------------------------------------------------------------------
-void TECExpert::tecPIDMinSet() {
-  for(auto it: fMapTecControlVoltageSet) {
-    QString sval = it.second->text();
-    float xval = sval.toFloat();
-    cout << "xval = " << xval << " it.first = " << it.first << endl;
-    fThread->setTECRegister(it.first, "PID_Min", xval);
-  }
-}
+//// -------------------------------------------------------------------------------
+//void TECExpert::tecPIDMinSet() {
+//  for(auto it: fMapTecControlVoltageSet) {
+//    QString sval = it.second->text();
+//    float xval = sval.toFloat();
+//    cout << "xval = " << xval << " it.first = " << it.first << endl;
+//    fThread->setTECRegister(it.first, "PID_Min", xval);
+//  }
+//}
 
 
-// -------------------------------------------------------------------------------
-void TECExpert::tecRefUSet() {
-  for(auto it: fMapTecControlVoltageSet) {
-    QString sval = it.second->text();
-    float xval = sval.toFloat();
-    cout << "xval = " << xval << " it.first = " << it.first << endl;
-    fThread->setTECRegister(it.first, "Ref_U", xval);
-  }
-}
+//// -------------------------------------------------------------------------------
+//void TECExpert::tecRefUSet() {
+//  for(auto it: fMapTecControlVoltageSet) {
+//    QString sval = it.second->text();
+//    float xval = sval.toFloat();
+//    cout << "xval = " << xval << " it.first = " << it.first << endl;
+//    fThread->setTECRegister(it.first, "Ref_U", xval);
+//  }
+//}
--- test1/TECExpert.h
+++ test1/TECExpert.h
@@ -20,6 +20,15 @@ public:
   void tecSetFromUI(int itec, std::string rname, QLineEdit *);
 
 public slots:
+  void tec1ModeSet() {tecSetFromUI(1, "Mode", fMapTecMode[1]); }
+  void tec2ModeSet() {tecSetFromUI(2, "Mode", fMapTecMode[2]); }
+  void tec3ModeSet() {tecSetFromUI(3, "Mode", fMapTecMode[3]); }
+  void tec4ModeSet() {tecSetFromUI(4, "Mode", fMapTecMode[4]); }
+  void tec5ModeSet() {tecSetFromUI(5, "Mode", fMapTecMode[5]); }
+  void tec6ModeSet() {tecSetFromUI(6, "Mode", fMapTecMode[6]); }
+  void tec7ModeSet() {tecSetFromUI(7, "Mode", fMapTecMode[7]); }
+  void tec8ModeSet() {tecSetFromUI(8, "Mode", fMapTecMode[8]); }
+
   void tec1VoltageSet() {tecSetFromUI(1, "ControlVoltage_Set", fMapTecControlVoltageSet[1]); }
   void tec2VoltageSet() {tecSetFromUI(2, "ControlVoltage_Set", fMapTecControlVoltageSet[2]); }
   void tec3VoltageSet() {tecSetFromUI(3, "ControlVoltage_Set", fMapTecControlVoltageSet[3]); }
@@ -29,14 +38,59 @@ public slots:
   void tec7VoltageSet() {tecSetFromUI(7, "ControlVoltage_Set", fMapTecControlVoltageSet[7]); }
   void tec8VoltageSet() {tecSetFromUI(8, "ControlVoltage_Set", fMapTecControlVoltageSet[8]); }
 
+  void tec1PIDkpSet() {tecSetFromUI(1, "PID_kp", fMapTecPIDkp[1]); }
+  void tec2PIDkpSet() {tecSetFromUI(2, "PID_kp", fMapTecPIDkp[2]); }
+  void tec3PIDkpSet() {tecSetFromUI(3, "PID_kp", fMapTecPIDkp[3]); }
+  void tec4PIDkpSet() {tecSetFromUI(4, "PID_kp", fMapTecPIDkp[4]); }
+  void tec5PIDkpSet() {tecSetFromUI(5, "PID_kp", fMapTecPIDkp[5]); }
+  void tec6PIDkpSet() {tecSetFromUI(6, "PID_kp", fMapTecPIDkp[6]); }
+  void tec7PIDkpSet() {tecSetFromUI(7, "PID_kp", fMapTecPIDkp[7]); }
+  void tec8PIDkpSet() {tecSetFromUI(8, "PID_kp", fMapTecPIDkp[8]); }
 
-  void tecModeSet();
-  void tecPIDkpSet();
-  void tecPIDkiSet();
-  void tecPIDkdSet();
-  void tecPIDMaxSet();
-  void tecPIDMinSet();
-  void tecRefUSet();
+  void tec1PIDkiSet() {tecSetFromUI(1, "PID_ki", fMapTecPIDki[1]); }
+  void tec2PIDkiSet() {tecSetFromUI(2, "PID_ki", fMapTecPIDki[2]); }
+  void tec3PIDkiSet() {tecSetFromUI(3, "PID_ki", fMapTecPIDki[3]); }
+  void tec4PIDkiSet() {tecSetFromUI(4, "PID_ki", fMapTecPIDki[4]); }
+  void tec5PIDkiSet() {tecSetFromUI(5, "PID_ki", fMapTecPIDki[5]); }
+  void tec6PIDkiSet() {tecSetFromUI(6, "PID_ki", fMapTecPIDki[6]); }
+  void tec7PIDkiSet() {tecSetFromUI(7, "PID_ki", fMapTecPIDki[7]); }
+  void tec8PIDkiSet() {tecSetFromUI(8, "PID_ki", fMapTecPIDki[8]); }
+
+  void tec1PIDkdSet() {tecSetFromUI(1, "PID_kd", fMapTecPIDkd[1]); }
+  void tec2PIDkdSet() {tecSetFromUI(2, "PID_kd", fMapTecPIDkd[2]); }
+  void tec3PIDkdSet() {tecSetFromUI(3, "PID_kd", fMapTecPIDkd[3]); }
+  void tec4PIDkdSet() {tecSetFromUI(4, "PID_kd", fMapTecPIDkd[4]); }
+  void tec5PIDkdSet() {tecSetFromUI(5, "PID_kd", fMapTecPIDkd[5]); }
+  void tec6PIDkdSet() {tecSetFromUI(6, "PID_kd", fMapTecPIDkd[6]); }
+  void tec7PIDkdSet() {tecSetFromUI(7, "PID_kd", fMapTecPIDkd[7]); }
+  void tec8PIDkdSet() {tecSetFromUI(8, "PID_kd", fMapTecPIDkd[8]); }
+
+  void tec1PIDMaxSet() {tecSetFromUI(1, "PID_Max", fMapTecPIDMax[1]); }
+  void tec2PIDMaxSet() {tecSetFromUI(2, "PID_Max", fMapTecPIDMax[2]); }
+  void tec3PIDMaxSet() {tecSetFromUI(3, "PID_Max", fMapTecPIDMax[3]); }
+  void tec4PIDMaxSet() {tecSetFromUI(4, "PID_Max", fMapTecPIDMax[4]); }
+  void tec5PIDMaxSet() {tecSetFromUI(5, "PID_Max", fMapTecPIDMax[5]); }
+  void tec6PIDMaxSet() {tecSetFromUI(6, "PID_Max", fMapTecPIDMax[6]); }
+  void tec7PIDMaxSet() {tecSetFromUI(7, "PID_Max", fMapTecPIDMax[7]); }
+  void tec8PIDMaxSet() {tecSetFromUI(8, "PID_Max", fMapTecPIDMax[8]); }
+
+  void tec1PIDMinSet() {tecSetFromUI(1, "PID_Min", fMapTecPIDMin[1]); }
+  void tec2PIDMinSet() {tecSetFromUI(2, "PID_Min", fMapTecPIDMin[2]); }
+  void tec3PIDMinSet() {tecSetFromUI(3, "PID_Min", fMapTecPIDMin[3]); }
+  void tec4PIDMinSet() {tecSetFromUI(4, "PID_Min", fMapTecPIDMin[4]); }
+  void tec5PIDMinSet() {tecSetFromUI(5, "PID_Min", fMapTecPIDMin[5]); }
+  void tec6PIDMinSet() {tecSetFromUI(6, "PID_Min", fMapTecPIDMin[6]); }
+  void tec7PIDMinSet() {tecSetFromUI(7, "PID_Min", fMapTecPIDMin[7]); }
+  void tec8PIDMinSet() {tecSetFromUI(8, "PID_Min", fMapTecPIDMin[8]); }
+
+  void tec1RefUSet() {tecSetFromUI(1, "Ref_U", fMapTecRefU[1]); }
+  void tec2RefUSet() {tecSetFromUI(2, "Ref_U", fMapTecRefU[2]); }
+  void tec3RefUSet() {tecSetFromUI(3, "Ref_U", fMapTecRefU[3]); }
+  void tec4RefUSet() {tecSetFromUI(4, "Ref_U", fMapTecRefU[4]); }
+  void tec5RefUSet() {tecSetFromUI(5, "Ref_U", fMapTecRefU[5]); }
+  void tec6RefUSet() {tecSetFromUI(6, "Ref_U", fMapTecRefU[6]); }
+  void tec7RefUSet() {tecSetFromUI(7, "Ref_U", fMapTecRefU[7]); }
+  void tec8RefUSet() {tecSetFromUI(8, "Ref_U", fMapTecRefU[8]); }
 
 private:
   driveHardware *fThread;
