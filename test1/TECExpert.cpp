0255756b730acb0bd33dc5777a989595091f212b
Urs Langenegger <urslangenegger@gmail.com>
2023-09-07 08:38:31 +0200

    add more regs; move to map

--- test1/TECExpert.cpp
+++ test1/TECExpert.cpp
@@ -13,8 +13,18 @@ using namespace std;
 // -------------------------------------------------------------------------------
 TECExpert::TECExpert(QWidget *, driveHardware *x) : fThread(x) {
   fUI = new QWidget();
+
   int height(35);
-  fUI->setFixedSize(QSize(500, (1+5)*height));
+  map<int, QString> vregs{{1,"Mode"},
+                         {2,"Control_Voltage"},
+                         {3,"PID_kp"},
+                         {4,"PID_ki"},
+                         {5,"PID_kd"},
+                         {6,"PID_Max"},
+                         {7,"PID_Min"},
+                         {8,"Ref_U"}};
+
+  fUI->setFixedSize(QSize(600, vregs.size()*height));
 
   QPushButton *quitButton = new QPushButton("Close", fUI);
   connect(quitButton, &QPushButton::clicked, this, &TECExpert::close);
@@ -30,52 +40,20 @@ TECExpert::TECExpert(QWidget *, driveHardware *x) : fThread(x) {
     leftLayout->addWidget(b, 0, i);
   }
 
-  b = new QLabel(fUI);
-  b->setText("Mode");
-  leftLayout->addWidget(b, 1, 0);
-
-  b = new QLabel(fUI);
-  b->setText("ControlVoltage_Set");
-  leftLayout->addWidget(b, 2, 0);
-
-  b = new QLabel(fUI);
-  b->setText("PID_kp");
-  leftLayout->addWidget(b, 3, 0);
-
-  b = new QLabel(fUI);
-  b->setText("PID_ki");
-  leftLayout->addWidget(b, 4, 0);
-
-  b = new QLabel(fUI);
-  b->setText("PID_kd");
-  leftLayout->addWidget(b, 5, 0);
+  for (auto it: vregs) {
+      b = new QLabel(fUI);
+      b->setText(it.second);
+      leftLayout->addWidget(b, it.first, 0);
+  }
 
   leftLayout->addWidget(quitButton, 0, 0);
 
   QLineEdit *pte;
   for (int ix = 1; ix <= 8; ++ix) {
-      for (int iy = 1; iy <= 5; ++iy) {
+      for (int iy = 1; iy <= 8; ++iy) {
         pte = new QLineEdit(fUI);
         pte->setMaximumHeight(height);
-        switch (iy) {
-        case 1:
-            pte->setText(QString::number(fThread->getTECRegister(ix, "Mode")));
-            break;
-        case 2:
-            pte->setText(QString::number(fThread->getTECRegister(ix, "ControlVoltage_Set")));
-            break;
-        case 3:
-            pte->setText(QString::number(fThread->getTECRegister(ix, "PID_kp")));
-            break;
-        case 4:
-            pte->setText(QString::number(fThread->getTECRegister(ix, "PID_ki")));
-            break;
-        case 5:
-            pte->setText(QString::number(fThread->getTECRegister(ix, "PID_kd")));
-            break;
-        default:
-            break;
-        }
+        pte->setText(QString::number(fThread->getTECRegister(ix, vregs[iy].toStdString())));
         leftLayout->addWidget(pte, iy, ix);
       }
   }
