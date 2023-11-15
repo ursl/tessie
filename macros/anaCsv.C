af978d685ae1e2d885055047a569f7941ddd9598
Urs Langenegger <urslangenegger@gmail.com>
2023-11-15 13:59:07 +0100

    HDI PT 1000 temperature study

--- macros/anaCsv.C
+++ macros/anaCsv.C
@@ -329,3 +329,151 @@ void corrTemps(string filename = "/Users/ursl/data/tessie/231103-tessie.csv") {
   fOut->Close();
   
 }
+
+
+// ----------------------------------------------------------------------
+void corrTempM2TempHDI(string filename = "/Users/ursl/data/tessie/231103b-tessie.csv") {
+
+  shrinkPad(0.1, 0.12);
+  
+  vector<csvEntry> csv;
+  if (!csvRead) csv = readCsv(filename);
+
+  int nstart(csv[0].time.Convert()),
+    nend(csv[csv.size()-1].time.Convert()),
+    nbin(nend-nstart+1);
+  
+  vector<TGraph*> vg;
+  for (int it = 0; it < 8; ++it) {
+    TGraph *g1;
+    vg.push_back(g1 = new TGraph());
+    g1->SetName(Form("gtempM%d", it+1));
+    
+    if (0 == it) g1->SetMarkerColor(kBlue);
+    if (1 == it) g1->SetMarkerColor(kBlue+3);
+    if (2 == it) g1->SetMarkerColor(kRed);
+    if (3 == it) g1->SetMarkerColor(kRed+2);
+    if (4 == it) g1->SetMarkerColor(kGreen+1);
+    if (5 == it) g1->SetMarkerColor(kGreen+3);
+    if (6 == it) g1->SetMarkerColor(kOrange+1);
+    if (7 == it) g1->SetMarkerColor(kMagenta);
+
+    if (0 == it) g1->SetLineColor(kBlue);
+    if (1 == it) g1->SetLineColor(kBlue+3);
+    if (2 == it) g1->SetLineColor(kRed);
+    if (3 == it) g1->SetLineColor(kRed+2);
+    if (4 == it) g1->SetLineColor(kGreen+1);
+    if (5 == it) g1->SetLineColor(kGreen+3);
+    if (6 == it) g1->SetLineColor(kOrange+1);
+    if (7 == it) g1->SetLineColor(kMagenta);
+  }
+
+  
+  for (unsigned int i = 0; i < csv.size(); ++i) {
+    int ntime = csv[i].time.Convert() - nstart;
+    int atime = csv[i].time.Convert();
+    cout << csv[i].time.AsString()
+         << " convert() = " << ntime
+         << " tempM[3] = " << csv[i].tempM[3]
+         << " absolute time = " << atime
+         << endl;
+    for (int it = 0; it < 8; ++it) {
+      vg[it]->AddPoint(atime, csv[i].tempM[it]);
+    }                   
+  }
+
+  TLegend *tleg = newLegend(0.15, 0.15, 0.4, 0.5);
+  for (int it = 0; it < 8; ++it) {
+    TLegendEntry *tle(0);
+    if (it == 0) {
+      tle = tleg->AddEntry(vg[it], Form("HDI PT1000"));
+    } else if (it == 7) {
+      tle = tleg->AddEntry(vg[it], Form("TEC %d", it+1));
+    } else {
+      tle = tleg->AddEntry(vg[it], Form("TEC %d (empty)", it+1));
+    }
+    tle->SetTextSize(0.03);
+    if (0 == it) {
+      vg[it]->Draw("alp");
+      vg[it]->GetXaxis()->SetTitle("time");
+      vg[it]->GetYaxis()->SetTitle("Temperature [C]");
+    } else {
+      vg[it]->Draw("lp");
+    }
+  }
+  
+  tleg->SetHeader("2023/11/03");
+  tleg->Draw();
+  vg[0]->GetXaxis()->SetTimeDisplay(1);
+  vg[0]->GetXaxis()->SetLimits(1699018796, 1699022316);
+  // 0]->GetXaxis()->SetLimits(1699028500.);
+  vg[0]->SetMaximum(40.);
+  vg[0]->SetMinimum(-48.);
+  
+  tl->SetTextSize(0.03);
+  tl->DrawLatex(0.80, 0.91, "NP/UL");
+
+  pl->SetNDC(kTRUE);
+  pl->DrawLine(0.2, 0., 0.2, 1.);
+  
+  string pdfname = "temperaturesHDI-" + filename.substr(filename.rfind("/")+1);
+  replaceAll(pdfname, ".csv", ".pdf");
+  c0.SaveAs(pdfname.c_str());
+
+  TGraph *gcorr = new TGraph();
+  
+  int ix, tx, ti;
+  for (int i = 0; i < vg[0]->GetN(); ++i) {
+    ix = vg[0]->GetPointX(i);
+
+    for (int t = 0; t < vg[7]->GetN(); ++t) {
+      tx = vg[7]->GetPointX(t);
+      if (tx == ix) {
+        ti = t;
+        break;
+      }
+    }
+    
+    double iy = vg[0]->GetPointY(i);
+    double ty = vg[7]->GetPointY(ti);
+    gcorr->AddPoint(iy, ty);
+    //  m->SetMarkerColor(i);;
+    cout << ix << "/" << tx << " date: " << ":  iy = " << iy << " ty = " << ty << endl;
+  }
+
+  c0.Clear();
+  gcorr->SetMarkerStyle(24);
+  gcorr->SetMarkerSize(0.6);
+  gcorr->Draw("ap");
+  gcorr->GetXaxis()->SetTitle("HDI temperature [C]");
+  gcorr->GetYaxis()->SetTitle("TEC8 temperature [C]");
+
+  tl->SetTextSize(0.03);
+  tl->DrawLatex(0.80, 0.91, "NP/UL");
+
+  
+  if (0) {
+    double x, y;
+    TMarker *m(0); 
+    for (int i = 1; i < gcorr->GetN(); ++i) {
+      gcorr->GetPoint(i, x, y);
+      m = new TMarker(x, y, 24);
+      cout << "Marker at " << x << "/" << y << endl;
+      if (i < 100) {
+        m->SetMarkerColor(kRed);
+      } else if (i < 200) {
+        m->SetMarkerColor(kBlue);
+      } else {
+        m->SetMarkerColor(kGreen);
+      }
+      m->SetMarkerSize(1);
+      //    m->DrawMarker(x, y);
+      m->Draw();
+    }
+  }
+
+  string pdfname2 = "corrHDI-" + filename.substr(filename.rfind("/")+1);
+  replaceAll(pdfname2, ".csv", ".pdf");
+  c0.SaveAs(pdfname2.c_str());
+
+}
