4a159e81794b4770553d47f0aa2ccead7a3e5f79
Urs Langenegger <urslangenegger@gmail.com>
2023-11-21 16:55:33 +0100

    node.js/socket.io communication between server and client works in both ways

---
+++ node/test1/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+var valve0Status = 0;
+var valve1Status = 0;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+    
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    socket.on('valve0', (msg) => {
+        if (valve0Status == 0) {
+            valve0Status = 1;
+        } else {
+            valve0Status = 0;
+        }
+        console.log('valve0 clicked, valve0Status = ' + valve0Status);
+        if (valve0Status == 0) {
+            socket.emit('valve0white');
+        } else {
+            socket.emit('valve0blue');
+        }
+    });
+
+    socket.on('valve1', (msg) => {
+        if (valve1Status == 0) {
+            valve1Status = 1;
+        } else {
+            valve1Status = 0;
+        }
+        console.log('valve1 clicked, valve1Status = ' + valve1Status);
+        if (valve1Status == 0) {
+            socket.emit('valve1white');
+        } else {
+            socket.emit('valve1blue');
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
