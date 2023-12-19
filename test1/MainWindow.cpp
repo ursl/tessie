1858eb2c4375775d3abd9e7feb0edc9a3dc13249
Urs Langenegger <urslangenegger@gmail.com>
2023-12-19 15:09:55 +0100

    remove audio playing for software i2c

--- test1/MainWindow.cpp
+++ test1/MainWindow.cpp
@@ -426,7 +426,7 @@ void MainWindow::updateHardwareDisplay() {
   if (fpHw->redCANErrors() > 0) {
     cout << "Setting CANbus error counter line edit to red" << endl;
     ui->lineEditCANbusError->setStyleSheet("QLineEdit {background-color : red; }");
-    system("/usr/bin/cvlc --play-and-exit houstonwehaveaproblem_loud.mp3 &");
+    //    system("/usr/bin/cvlc --play-and-exit houstonwehaveaproblem_loud.mp3 &");
   } else {
     ui->lineEditCANbusError->setStyleSheet("QLineEdit {background-color : white; }");
   }
@@ -434,7 +434,7 @@ void MainWindow::updateHardwareDisplay() {
   if (fpHw->redI2CErrors() > 0) {
     cout << "Setting I2C error counter line edit to red" << endl;
     ui->lineEditI2CError->setStyleSheet("QLineEdit {background-color : red; }");
-    system("/usr/bin/cvlc --play-and-exit houstonwehaveaproblem_loud.mp3  &");
+    //    system("/usr/bin/cvlc --play-and-exit houstonwehaveaproblem_loud.mp3  &");
   } else {
     ui->lineEditI2CError->setStyleSheet("QLineEdit {background-color : white; }");
   }
