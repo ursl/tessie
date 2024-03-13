655955c592895d3f0d541558ff03bcf027dc640a
Urs Langenegger <urslangenegger@gmail.com>
2024-03-13 08:55:13 +0100

    make sure that loopback is off

--- resetCAN.sh
+++ resetCAN.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ip link set can0 down; sudo ip link set can0 type can bitrate 125000; sudo ip link set can0 up
+sudo ip link set can0 down; sudo ip link set can0 type can bitrate 125000 loopback off; sudo ip link set can0 up
