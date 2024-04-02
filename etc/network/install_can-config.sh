daa2d288e0497ff11d2bfde7dc1f866fb03a9300
Branislav Ristic <bristic@cern.ch>
2024-04-02 14:43:03 +0200

    handle missing CAN interface

--- etc/network/install_can-config.sh
+++ etc/network/install_can-config.sh
@@ -3,4 +3,4 @@
 cd "$(dirname "$0")"
 
 sudo cp interfaces.d/can0 /etc/network/interfaces.d
-sudo ifup can0
+sudo ifup can0 || echo "!!! Interface can0 not found. It should show up after reboot and if the coldbox RPi hat is installed"
