7df6617679d0c676e217674a83d73b91b3b8da3b
Urs Langenegger <urslangenegger@gmail.com>
2024-04-05 17:23:02 +0200

    hike version string

--- test1/MainWindow.cpp
+++ test1/MainWindow.cpp
@@ -21,7 +21,7 @@ MainWindow::MainWindow(tLog &x, driveHardware *h, QWidget *parent) :
   ui->setupUi(this);
 
 
-  ui->labelVersion->setText("2024/03/15-01");
+  ui->labelVersion->setText("2024/04/05-01");
   //ui->labelStatus->setText("OK");
 
   ui->lineEditRunTime->setAlignment(Qt::AlignRight);
