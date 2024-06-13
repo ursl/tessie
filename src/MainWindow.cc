5db59634b52d38f711b5b2cee47614707c7c9e14
Urs Langenegger <urslangenegger@gmail.com>
2024-06-13 16:28:07 +0200

    basic design is there

--- src/MainWindow.cc
+++ src/MainWindow.cc
@@ -26,14 +26,12 @@ MainWindow::MainWindow(tLog &x, driveHardware *h, QWidget *parent) :
 
   QWidget *wdg = new QWidget(this);
   wdg->setFocusPolicy(Qt::NoFocus);
-
+  // -- Top vertical stack of layouts
   QVBoxLayout *vlayTop = new QVBoxLayout(wdg);
 
+  // -- top row, containing (left) info block and (right) env block
   QHBoxLayout *hlay0 = new QHBoxLayout(wdg);
   vlayTop->addLayout(hlay0);
-
-  QVBoxLayout *vlay00 = new QVBoxLayout(wdg);
-  QVBoxLayout *vlay01 = new QVBoxLayout(wdg);
 
   // -- Info block
   QGridLayout *glay00 = new QGridLayout();
@@ -54,7 +52,6 @@ MainWindow::MainWindow(tLog &x, driveHardware *h, QWidget *parent) :
   getline(INS, sline);
   fqleVersion->setText(sline.c_str());
   INS.close();
-
 
   glay00->addWidget(lblA,  0, 0, 1, 1);
   glay00->addWidget(fqleCANbusErrors, 0, 1, 1, 1);
@@ -92,28 +89,58 @@ MainWindow::MainWindow(tLog &x, driveHardware *h, QWidget *parent) :
 
   hlay0->addLayout(glay01);
 
-  hlay0->addLayout(vlay00);
 
-
+  // -- bottom row, containing (left) buttons and (right) TEC array
   QHBoxLayout *hlay1 = new QHBoxLayout(wdg);
   vlayTop->addLayout(hlay1);
 
-  QPushButton *btn1 = new QPushButton("btn1"); btn1->setFocusPolicy(Qt::NoFocus);
-  btn1->setFont(fFont1);
-  vlay01->addWidget(btn1);
-
-
-  QPushButton *btn3 = new QPushButton("Quit"); btn3->setFocusPolicy(Qt::NoFocus);
-  btn3->setFont(fFont1);
-  connect(btn3, &QPushButton::clicked, this, &MainWindow::btnQuit);
-  vlay00->addWidget(btn3);
+  // -- buttons
+  const QSize btnSize = QSize(100, 50);
+  QVBoxLayout *vlay00 = new QVBoxLayout(wdg);
 
   QPushButton *btn4 = new QPushButton("Flush"); btn4->setFocusPolicy(Qt::NoFocus);
   btn4->setFont(fFont1);
+  btn4->setFixedSize(btnSize);
   vlay00->addWidget(btn4);
+
   QPushButton *btn5 = new QPushButton("Rinse"); btn5->setFocusPolicy(Qt::NoFocus);
   btn5->setFont(fFont1);
+  btn5->setFixedSize(btnSize);
   vlay00->addWidget(btn5);
+
+  QPushButton *btn3 = new QPushButton("Quit"); btn3->setFocusPolicy(Qt::NoFocus);
+  btn3->setFont(fFont1);
+  btn3->setFixedSize(btnSize);
+  connect(btn3, &QPushButton::clicked, this, &MainWindow::btnQuit);
+  vlay00->addWidget(btn3);
+
+  hlay1->addLayout(vlay00);
+
+
+  // -- TEC block
+  QGridLayout *glay02 = new QGridLayout();
+  wdg->setLayout(glay02);
+
+  QLabel *lbl0 = new QLabel("0"); setupLBL(lbl0);
+  QLabel *lbl1 = new QLabel("1"); setupLBL(lbl1);
+  QLabel *lbl2 = new QLabel("2"); setupLBL(lbl2);
+  QLabel *lbl3 = new QLabel("3"); setupLBL(lbl3);
+  QLabel *lbl4 = new QLabel("4"); setupLBL(lbl4);
+  QLabel *lbl5 = new QLabel("5"); setupLBL(lbl5);
+  QLabel *lbl6 = new QLabel("6"); setupLBL(lbl6);
+  QLabel *lbl7 = new QLabel("7"); setupLBL(lbl7);
+
+  glay02->addWidget(lbl0,  0, 0, 1, 1);
+  glay02->addWidget(lbl1,  0, 1, 1, 1);
+  glay02->addWidget(lbl2,  0, 2, 1, 1);
+  glay02->addWidget(lbl3,  0, 3, 1, 1);
+
+  glay02->addWidget(lbl4,  1, 0, 1, 1);
+  glay02->addWidget(lbl5,  1, 1, 1, 1);
+  glay02->addWidget(lbl6,  1, 2, 1, 1);
+  glay02->addWidget(lbl7,  1, 3, 1, 1);
+
+  hlay1->addLayout(glay02);
 
   setCentralWidget(wdg);
 
@@ -137,8 +164,6 @@ void MainWindow::updateHardwareDisplay() {
   fqleWT->setText(QString::number(fpHw->getTECRegister(8, "Temp_W"), 'f', 2));
   fqleRH->setText(QString::number(fpHw->getRH(), 'f', 2));
   fqleDP->setText(QString::number(fpHw->getDP(), 'f', 2));
-
-
 }
 
 // ----------------------------------------------------------------------
