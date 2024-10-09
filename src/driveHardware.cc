1a80d6deae7f118e7ceb7c1d881d6af27dd8a32b
Urs Langenegger <urslangenegger@gmail.com>
2024-10-09 10:49:13 +0200

    wait before entering loop (to give other threads time to start up completely)

--- src/driveHardware.cc
+++ src/driveHardware.cc
@@ -296,7 +296,8 @@ void driveHardware::sentFromServer(QString msg) {
 
 // ----------------------------------------------------------------------
 void driveHardware::doRun() {
-  cout << "driveHardware::doRun() entered" << endl;
+  cout << "driveHardware::doRun() entered, wait 5 seconds" << endl;
+  std::this_thread::sleep_for(std::chrono::milliseconds(5000));
   int cnt(-1);
   struct timeval tvVeryOld, tvOld, tvNew;
   gettimeofday(&tvVeryOld, 0);
