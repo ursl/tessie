68eb13e3189f41b76d85ba55b8d71851aa8aaf6e
Urs Langenegger <urslangenegger@gmail.com>
2024-10-09 10:49:50 +0200

    remove wait

--- resetCAN.sh
+++ resetCAN.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo ip link set can0 down; sudo ip link set can0 type can bitrate 125000 loopback off; sudo ip link set can0 up
-echo "sleep for 10 seconds"
-sleep 10
+#echo "sleep for 10 seconds"
+#sleep 10
