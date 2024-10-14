72bcf77c6fb7f3136b99088c9fa911350a560123
Urs Langenegger <urslangenegger@gmail.com>
2024-10-14 15:37:48 +0200

    add flowmeter readout

--- src/driveHardware.cc
+++ src/driveHardware.cc
@@ -2151,7 +2151,7 @@ void driveHardware::readFlowmeter() {
 
   char data = 0x0;
   length = i2c_read_device(fPiGPIO, handle, &data, 1);
-  data ~= data;
+  data = ~data;
   stringstream a("flowmeter readout data =  " + to_string(data));
   fLOG(WARNING, a.str());
   i2c_close(fPiGPIO, handle);
